e209d9c444d49111aa13718831a31a29d03d60a0
Fortune Bekee <fortubeks@gmail.com>
2024-02-06 22:19:44 +0100

    .

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,5 +14,8 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call([CountriesSeeder::class]);
+        $this->call([ExpenseCategoriesTableSeeder::class]);
+        $this->call([PackagesTableSeeder::class]);
+        $this->call([UsersTableSeeder::class]);
     }
 }
